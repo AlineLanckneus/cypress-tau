f6f00d809c1feb38b6a3493b9afd2a622e3f4f1b
Aline L <aline.lanckneus@live.be>
2020-03-13 17:02:02 +0100

    type

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
-it('should navigate tot the TodoMVC app', () => {
-  cy.visit('http://todomvc-app-for-testing.surge.sh/thisdoesnotexist');
+it('should be able to add a new todo to the list', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh');
+
+  cy.get('.old-todo').type('clean room{enter}');
 });
