f785f932c8c04452ff19976b8115b6e2853dc293
Aline L <aline.lanckneus@live.be>
2020-03-14 16:52:50 +0100

    create new file filtering

--- cypress/integration/todomvc.spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-it('should be able to add a new todo to the list', () => {
-  cy.visit('http://todomvc-app-for-testing.surge.sh');
-
-  cy.get('.old-todo').type('clean room{enter}');
-});
