386263c261cf43e1d2a86c1ef4fff24bc26125cd
Aline L <aline.lanckneus@live.be>
2020-03-14 19:44:51 +0100

    add applitools eyes

---
+++ cypress/integration/todomvc-visuals.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+import * as todoPage from '../page-objects/todo-page';
+describe('visual validation', () => {
+  before(() => todoPage.navigate());
+
+  beforeEach(() =>
+    cy.eyesOpen({
+      appName: 'TAU TodoMVC',
+      batchName: 'TAU TodoMVC Hey!',
+      browser: [
+        { name: 'chrome', width: 1024, height: 768 },
+        { name: 'chrome', width: 800, height: 600 },
+        { name: 'firefox', width: 1024, height: 768 },
+        { deviceName: 'iPhone X' }
+      ]
+    })
+  );
+  afterEach(() => cy.eyesClose());
+  it('should look good', () => {
+    cy.eyesCheckWindow('empty todo list');
+    todoPage.addTodo('Clean room');
+    todoPage.addTodo('Learn javascript');
+
+    cy.eyesCheckWindow('two todos');
+
+    todoPage.toggleTodo(0);
+
+    cy.eyesCheckWindow('mark as completed');
+  });
+});
